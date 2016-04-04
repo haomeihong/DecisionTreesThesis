0f8f9e5696a1305a5440a7220488986ffe6554dd
Joris <Joris@jorislaptop.be>
2016-04-04 18:57:32 +0200

    Examples added

--- DecisionTreesThesis/src/test/test.java
+++ DecisionTreesThesis/src/test/test.java
@@ -1,9 +1,13 @@
 package test;
 
+import weka.classifiers.trees.*;
+
 public class test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		J48 classifier = new J48();
+		J48.main(args);
 	}
 
 }
